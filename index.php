73600e96937834e23e717ca3f1e8ed1af0ae4490
Mister_Bert0ni <corpofhack@gmail.com>
2022-11-25 15:18:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,84 @@
 <?php
+?>
 
-echo "Hello World!";
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"><style>      p        {FONT-SIZE: 14px; FONT-FAMILY: Arial; COLOR: #464547; MARGIN: 0px; LINE-HEIGHT: 20px; -webkit-text-size-adjust: none}     p.sign    {FONT-SIZE: 13px; FONT-FAMILY: arial; COLOR: #999999; MARGIN: 0px; LINE-HEIGHT: 18px; -webkit-text-size-adjust: none}     li        {FONT-SIZE: 14px; FONT-FAMILY: Arial; COLOR: #464547; MARGIN: 0px; LINE-HEIGHT: 20px; -webkit-text-size-adjust: none}     a        {FONT-SIZE: 14px; TEXT-DECORATION: none; FONT-FAMILY: Arial; COLOR: #1a9cb0; MARGIN: 0px; LINE-HEIGHT: 18px; -webkit-text-size-adjust: none}     td.1     {} </style>       <table style="FONT-FAMILY: Arial; TEXT-ALIGN: left; BORDER-SPACING: 0px; BACKGROUND-COLOR: #dddddd" width="100%" height="100%" cellspacing="0" cellpadding="0" border="0">         <tbody>
+  <tr>
+  <td width="34%">&nbsp;</td>
+  <td width="32%">
+      <table style="FONT-FAMILY: Arial; TEXT-ALIGN: left; MARGIN: 0px; BORDER-SPACING: 0" width="600" cellspacing="0" cellpadding="0" border="0" align="left">
+  <!-- Header -->
+          <tbody>
+
+   <tr style="COLOR: #464547; BACKGROUND-COLOR: #ffffff">
+
+       <td style="HEIGHT: 59px; MARGIN: 0px"><a style="HEIGHT: 59px; DISPLAY: block" target="_blank" href="http://support.epam.com/"><img style="display: block;" alt="support" src="http://static.cdn.epam.com/images/icons/esp/notification/support.epam.com.gray.png" class="" width="600" height="59"></a></td>
+
+   </tr>
+  <!-- End of Header -->
+  <tr style="COLOR: #464547; BACKGROUND-COLOR: #ffffff">
+      <td style="PADDING-BOTTOM: 0px; PADDING-TOP: 10px; PADDING-LEFT: 20px; PADDING-RIGHT: 20px; BACKGROUND-COLOR: #ffffff">
+      <table style="FONT-FAMILY: Arial; BORDER-COLLAPSE: collapse; PADDING-BOTTOM: 5px; TEXT-ALIGN: left; MARGIN: 0px; BORDER-SPACING: 0" width="100%" cellspacing="0" cellpadding="0" border="0" align="left">
+          <tbody>
+
+   <tr>
+
+       <td>
+
+
+</td></tr></tbody><tbody>
+
+
+    <tr>
+
+
+        <td style="BACKGROUND: #B22746; COLOR: #ffffff; PADDING-BOTTOM: 10px; PADDING-TOP: 10px; PADDING-LEFT: 10px; PADDING-RIGHT: 10px"><h1 style="FONT-SIZE: 14px; FONT-FAMILY: Arial; BACKGROUND: #B22746; COLOR: #ffffff; PADDING-BOTTOM: 0px; PADDING-TOP: 0px; PADDING-LEFT: 0px; MARGIN: 0px; LINE-HEIGHT: 18px; PADDING-RIGHT: 0px; -webkit-text-size-adjust: none">Critical Vulnerability Confirmed<br></h1></td>
+
+
+    </tr>
+
+
+</tbody>
+
+
+
+
+
+
+
+    </table>
+      </td>
+  </tr>
+  <!-- Intro Text -->
+  <tr style="COLOR: #464547; BACKGROUND-COLOR: #ffffff">
+      <td style="PADDING-BOTTOM: 10px; PADDING-TOP: 0px; PADDING-LEFT: 20px; PADDING-RIGHT: 20px; BACKGROUND-COLOR: #fff">
+          <table style="FONT-FAMILY: Arial; WIDTH: 560px; BORDER-COLLAPSE: collapse; TEXT-ALIGN: left; MARGIN: auto; BORDER-SPACING: 0" cellspacing="0" cellpadding="0" border="0" align="left">
+          <!-- Subtitle -->
+         <tbody>
+
+   <tr>
+
+       <td style="BORDER-BOTTOM: #eee 2px solid; PADDING-BOTTOM: 5px; PADDING-TOP: 15px; PADDING-LEFT: 0px; PADDING-RIGHT: 0px">
+
+<p>
+
+    Dear Team!</p><p><br>EPAM Security Team detected the critical security vulnerability.</p><p>This domain is vulnerable to subdomain takeoverr<br></p>
+
+       </td>
+
+   </tr>
+
+   <tr style="BACKGROUND-COLOR: #fff; BORDER-BOTTOM: #eee 2px solid; PADDING-BOTTOM: 5px; PADDING-TOP: 15px; PADDING-LEFT: 0px; PADDING-RIGHT: 0px">
+
+       <td style="COLOR: #464547; PADDING-BOTTOM: 5px; PADDING-TOP: 10px; PADDING-LEFT: 0px; PADDING-RIGHT: 0px">This domain was pointed to another domain name<br></td></tr><tr style="BACKGROUND-COLOR: #fff; PADDING-BOTTOM: 0px; PADDING-TOP: 15px; PADDING-LEFT: 0px; PADDING-RIGHT: 0px; BORDER-BOTTOM: #eee 2px solid"><td style="COLOR: #464547; PADDING-BOTTOM: 0px; PADDING-TOP: 10px; PADDING-LEFT: 0px; PADDING-RIGHT: 0px"><p><b><u>Important</u>: This issue should be address and fixed as soon as possible<br></b></p>
+
+       </td>
+
+   </tr> 						<tr> 							<td style="COLOR: #464547; PADDING-BOTTOM: 0px; PADDING-TOP: 10px; PADDING-LEFT: 0px; PADDING-RIGHT: 0px"> 	<p> 								<b>Note</b>: In case ignoring this issue the project will be isolated from network.<br></p> 							</td> 		</tr>
+          </tbody>
+          </table>
+      </td>
+  </tr>     <!-- Start Footer -->
+  <tr>
+      <td style="COLOR: #999999; PADDING-BOTTOM: 15px; PADDING-TOP: 15px; PADDING-LEFT: 20px; MARGIN: 0px; PADDING-RIGHT: 20px; BACKGROUND-COLOR: #464547">
+          <p class="sign">For additional information please see <a href="https://kb.epam.com/display/EPMSSDL/Quality+Gates+and+SLA">SLA</a> documentation<br></p></td>
+  </tr>     <!-- End of Footer -->         </tbody>     </table>     </td>     <td width="34%">&nbsp;</td>     </tr> </tbody> </table>
